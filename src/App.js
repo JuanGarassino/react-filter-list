d68cbdae891acc955cd508d03f34ce5c0b579e0a
Juan Carlos Garassino <jcgarassino@profile.es>
2019-11-02 09:46:02 +0100

    Adds Faker module. Playing with useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
+// Material modules
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
+// Faker module
+import Faker from 'faker';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -18,8 +19,12 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+let name = Faker.internet.userName();
+let name2 = Faker.internet.userName();
+
+
 function App() {
-  const [message, setMessage] = React.useState('Place your order');
+  const [list, setList] = useState(name);
   const classes = useStyles();
 
   return (
@@ -27,11 +32,11 @@ function App() {
       <header className="App-header">
 
       <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="component-simple">Search your analist</InputLabel>
+        <InputLabel htmlFor="component-simple">{list}</InputLabel>
         <Input id="component-simple" />
       </FormControl>
 
-      <Button variant="contained" color="primary" onClick={() => setMessage('Filter Applied')}>
+      <Button variant="contained" color="primary" onClick={() => setList(name2)}>
         Apply filter
       </Button>
 
