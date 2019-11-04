475965da321a34eddcf16cad166fac0348d603bb
Juan Carlos Garassino <jcgarassino@profile.es>
2019-11-04 12:55:18 +0100

    Gives functional structure. Next() Spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 // Material modules
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
+import Divider from '@material-ui/core/Divider';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-// Faker module
-import Faker from 'faker';
+import PhoneEnabledRoundedIcon from '@material-ui/icons/PhoneEnabledRounded';
+import PhoneDisabledRoundedIcon from '@material-ui/icons/PhoneDisabledRounded';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -17,30 +24,106 @@ const useStyles = makeStyles(theme => ({
   formControl: {
     margin: theme.spacing(1),
   },
+  offlineAvatar: {
+    margin: 10,
+    color: '#fff',
+    backgroundColor: '#d81b60',
+    cursor: 'not-available'
+  },
+  onlineAvatar: {
+    margin: 10,
+    color: '#fff',
+    backgroundColor: '#00acc1',
+    cursor: 'pointer',
+  },
 }));
+function sortByReview() {
+  console.log('Daleleeeee')
+}
 
-let name = Faker.internet.userName();
-let name2 = Faker.internet.userName();
+function AdvisorStatus({status}) {
+  const classes = useStyles();
+  if (status) {
+    return <PhoneEnabledRoundedIcon className={classes.onlineAvatar}/>
+  } else {
+    return <PhoneDisabledRoundedIcon className={classes.offlineAvatar}/>
+  }
+}
 
-
-function App() {
-  const [list, setList] = useState(name);
+function AdvisorsTable({advisors}) {
   const classes = useStyles();
 
+  return  <List className={classes.root}>
+            {advisors.map((advisor, i) => {
+              return  <AdvisorTableRow advisor={advisor} key={i}></AdvisorTableRow>        
+            })}
+          </List>
+}
+
+function AdvisorTableRow({advisor}) {
+
+    return   ( <article>
+                <ListItem key={advisor.id}>
+                  <AdvisorAvatar status={advisor.status}></AdvisorAvatar>
+                  <ListItemText primary={advisor.name + ' ' + advisor.surname}  secondary={
+                    <React.Fragment>
+                      is
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        color="textPrimary">
+                        {advisor.status ? ' available ' : ' not availabe '} 
+                      </Typography>
+                      right now and has {advisor.reviews} reviews
+                    </React.Fragment>
+                  } />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+               </article>
+    )
+}
+function AdvisorAvatar({status}) {
+const classes = useStyles();
+ return (
+    <ListItemAvatar>
+      <Avatar className={ status ? classes.onlineAvatar : classes.offlineAvatar}>
+        <AdvisorStatus status={status} />
+      </Avatar>
+    </ListItemAvatar>
+ )
+}
+
+function App() {
+  let [advisors, setAdvisors] = useState(['ajaja']);
+  const classes = useStyles();
+  
+  async function fetchData() {
+    const res = await fetch("http://localhost:3000/advisors");
+    let response = await res.json()
+    setAdvisors(response.advisors)
+  }
+  
+  useEffect(() => {
+    fetchData()
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="component-simple">{list}</InputLabel>
-        <Input id="component-simple" />
-      </FormControl>
-
-      <Button variant="contained" color="primary" onClick={() => setList(name2)}>
-        Apply filter
-      </Button>
-
+        <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="component-simple">Enter name</InputLabel>
+          <Input id="component-simple" />
+        </FormControl>
+          <Button variant="contained" color="primary" onClick={sortByReview()}>
+            Reviews
+          </Button>
+        </div>
       </header>
+      <section className="App-section">
+        <AdvisorsTable advisors={advisors}></AdvisorsTable>
+      </section>
+
     </div>
   );
 }
